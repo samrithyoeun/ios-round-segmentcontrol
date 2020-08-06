32ad818f2a25eecb5dca40adc9b8d301217a6497
sam yoeun <sam.yoeun@pathmazing.com>
2020-08-06 21:32:08 +0700

    Initial Commit

---
+++ ios-round-segmentcontrol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-round-segmentcontrol
+//
+//  Created by Samrith Yoeun on 8/6/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
