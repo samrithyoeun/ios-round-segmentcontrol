2214c7df5e7584356b28ab8d98f1d503b841994a
sam yoeun <sam.yoeun@pathmazing.com>
2020-08-06 21:45:09 +0700

    first commit

---
+++ ios-round-segmentcontrol/CustomSegmentControl.swift
@@ -0,0 +1,9 @@
+//
+//  CustomSegmentControl.swift
+//  ios-round-segmentcontrol
+//
+//  Created by Samrith Yoeun on 8/6/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import Foundation
